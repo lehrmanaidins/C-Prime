c4272c1c89dfd977f6bd33b60e9ba039df57e2fb
Aidin Lehrman <lehrmanaidin@gmail.com>
2026-09-08 11:57:08 -0400

    OperatorToken

--- src/lexer/tokens/operator.hpp
+++ src/lexer/tokens/operator.hpp
@@ -35,6 +35,51 @@ enum class OperatorType {
     Dollar
 };
 
+static const std::unordered_map<std::string, OperatorType> operator_map = {
+    {"+", OperatorType::Plus},
+    {"-", OperatorType::Minus},
+    {"*", OperatorType::Asterisk},
+    {"/", OperatorType::Slash},
+    {"%", OperatorType::Percent},
+    {"=", OperatorType::Equals},
+    {"<", OperatorType::LessThan},
+    {">", OperatorType::GreaterThan},
+    {"!", OperatorType::Exclamation},
+    {"&", OperatorType::Ampersand},
+    {"|", OperatorType::Pipe},
+    {"^", OperatorType::Caret},
+    {"~", OperatorType::Tilde},
+    {".", OperatorType::Dot},
+    {":", OperatorType::Colon},
+    {"?", OperatorType::Question},
+    {"(", OperatorType::LeftParenthesis},
+    {")", OperatorType::RightParenthesis},
+    {"[", OperatorType::LeftBracket},
+    {"]", OperatorType::RightBracket},
+    {"{", OperatorType::LeftBrace},
+    {"}", OperatorType::RightBrace},
+    {"@", OperatorType::At},
+    {"#", OperatorType::Octothorpe},
+    {"$", OperatorType::Dollar}
+};
+
 struct OperatorToken : Token<TokenType::Operator> {
     OperatorType operator_type;
+
+    OperatorToken(std::string lexeme, std::size_t line, std::size_t column, OperatorType operator_type)
+        : Token(lexeme, line, column),
+          operator_type(mapOperatorType(lexeme))
+    {}
+
+    static OperatorType mapOperatorType(const std::string& lexeme) {
+        auto it = operator_map.find(lexeme);
+        if (it != operator_map.end()) {
+            return it->second;
+        }
+        throw std::invalid_argument("Invalid operator lexeme: " + lexeme);
+    }
+
+    static bool isOperator(const std::string& lexeme) {
+        return operator_map.find(lexeme) != operator_map.end();
+    }
 };
