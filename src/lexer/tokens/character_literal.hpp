467849187ee8ad5cbdd4df7956b78d7cca52cbf3
Aidin Lehrman <lehrmanaidin@gmail.com>
2026-09-08 11:53:07 -0400

    CharacterLiteralToken

--- src/lexer/tokens/character_literal.hpp
+++ src/lexer/tokens/character_literal.hpp
@@ -13,4 +13,9 @@ enum class CharacterType {
 
 struct CharacterLiteralToken : Token<TokenType::CharacterLiteral> {
     CharacterType character_type;
+
+    CharacterLiteralToken(std::string lexeme, std::size_t line, std::size_t column)
+        : Token(lexeme, line, column),
+          character_type(CharacterType::Character)
+    {}
 };
